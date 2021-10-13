7131485755e145ccf8b138d213e6962da5426044
KatarinaSirocic <katarina.sirocic@vivifyideas.com>
2021-10-13 17:27:49 +0200

    Add more stuff

---
+++ cypress/fixtures/integration/board.spec.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+import createOrganization from "../fixtures/createOrganization.json"
+import loginPage from "../fixtures/loginPage.json"
+import data from "../fixtures/data.json"
+import navigation from "../fixtures/navigation.json"
+import myOrganizations from "../fixtures/myOrganizations.json"
+import createBoard from "../fixtures/createBoard.json"
+
+describe("CRUD Board", ()=>{
+
+    it("Login", ()=>{
+        cy.visit("/")
+        cy.get(loginPage.emailInput).type(data.user.email)
+        cy.get(loginPage.passwordInput).type(data.user.password)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Go to board creation", ()=>{
+        cy.get(navigation.myOrganizations).click()
+        cy.get(myOrganizations.organizationAvatar).click()
+    })
+
+    it("Start creating new board, then cancel it",()=>{
+        cy.get(createOrganization.okButton).click()
+        cy.get(createBoard.addNewBoard).click()
+        cy.get(createBoard.nameInput).type(data.board.boardName)
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.scrumRadioButton).click({force: true})
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.xButton).click()
+    })
+
+    it("Create new board",()=>{
+        cy.get(createBoard.addNewBoard).click()
+        cy.get(createBoard.nameInput).type(data.board.boardName)
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.scrumRadioButton).click({force: true})
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.nextButton).click()
+        cy.get(createBoard.nextButton).click()
+        cy.get(navigation.myOrganizations).click()
+        // cy.get(myOrganizations.getBoard).should('be.visible').click()
+    })
+
+    // it("Get board",()=>{
+    //     cy.get(navigation.myOrganizations).click()
+    //     cy.get(myOrganizations.getBoard).should('be.visible').click()
+    // })
+
+    it("Edit board details",()=>{
+        cy.get(createBoard.boradSideMenu.configureBoard.configureBoardButton).click()
+        cy.get(createBoard.boradSideMenu.configureBoard.boardTitleInput).clear().type(data.board.changedBoardName)
+        cy.get(createBoard.boradSideMenu.configureBoard.boardCodeInput).clear().type(data.board.boardCode)
+        cy.get(createBoard.boradSideMenu.configureBoard.boradDescription).clear().type(data.board.boardDescription)
+        cy.get(createBoard.boradSideMenu.configureBoard.updateBoardButton).click({force: true})
+    })
+
+    it("Start delete board, then cancel it",()=>{
+        cy.get(createBoard.boradSideMenu.configureBoard.deteleBoardButton).click()
+        cy.get(createBoard.boradSideMenu.configureBoard.cancelButton).click()
+    })
+
+    it("Delete board",()=>{
+        cy.get(createBoard.boradSideMenu.configureBoard.deteleBoardButton).click()
+        cy.get(createBoard.boradSideMenu.configureBoard.saveButton).click()
+        cy.get(createOrganization.okButton).click()
+    })
+})
---
+++ cypress/fixtures/integration/login.spec.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+const signUpPage = require("../fixtures/signUpPage.json")
+//import signUpPage from "../fixtures/signUpPage.json"
+import data from "../fixtures/data.json"
+import sidebar from "../fixtures/sidebar.json"
+import navigation from "../fixtures/navigation.json"
+import loginPage from "../fixtures/loginPage"
+
+
+describe("Login", () =>{
+
+    it("Visit url", () =>{
+        cy.visit("/", {timeout: 30000});
+    });
+
+    it("Login: Wrong password", () => {
+        cy.get(loginPage.emailInput).clear().type(data.user.email)
+        cy.get(loginPage.passwordInput).clear().type(data.invalidUser.wrongPassword)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Login: Empty password ", () => {
+        cy.get(loginPage.emailInput).clear().type(data.user.email)
+        cy.get(loginPage.passwordInput).clear().type(data.invalidUser.emptySpacePassword)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Login: Empty email field", () => {
+        cy.get(loginPage.emailInput).clear().type(data.invalidUser.emptyEmail)
+        cy.get(loginPage.passwordInput).clear().type(data.user.password)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Login: Wrong email", () => {
+        cy.get(loginPage.emailInput).clear().type(data.invalidUser.wrongEmail)
+        cy.get(loginPage.passwordInput).clear().type(data.invalidUser.wrongEmail)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Login: No body in email", () => {
+        cy.get(loginPage.emailInput).clear().type(data.invalidUser.noBodyemail)
+        cy.get(loginPage.passwordInput).clear().type(data.user.password)
+        cy.get(loginPage.logInButton).click()
+    })
+
+    it("Login: No gmail.com part", () => {
+        cy.get(loginPage.emailInput).clear().type(data.invalidUser.noGmailEmail)
+        cy.get(loginPage.passwordInput).clear().type(data.user.password)
+        cy.get(loginPage.logInButton).click()
+    })
+
+
+    it("Valid login", () => {
+        cy.get(loginPage.emailInput).clear().type(data.user.email)
+        cy.get(loginPage.passwordInput).clear().type(data.user.password)
+        cy.get(loginPage.logInButton).click()
+
+    })
+
+    it("Logout", () => {
+        cy.get(sidebar.myAccount.account).click({force: true})
+        cy.get(sidebar.myAccount.profile).click()
+        cy.get(navigation.logOutButton).click()
+    })
+})
